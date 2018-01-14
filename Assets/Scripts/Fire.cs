171ce2b39c5baa87d614283ae591bc6022566a8d
emontecalvo <emilymontecalvo@gmail.com>
2018-01-14 14:10:29 -0800

    initial commit

---
+++ Assets/Scripts/Fire.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour {
+
+	private static Fire _inst = null;
+
+	public static Fire inst ( get {
+		if (_inst == null) {
+			_inst = FindObjectOfType<Fire>();
+		}
+		return _inst;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
