46d9101eb2eafba568a0a9488d6b91f875091834
emontecalvo <emilymontecalvo@gmail.com>
2018-01-14 15:08:33 -0800

    adding tree/wood basic transition

--- Assets/Scripts/Jello.cs
+++ Assets/Scripts/Jello.cs
@@ -2,17 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Jello : MonoBehaviour {
+public class Jello : MonoBehaviour
+{
+	public KeyCode UpKey;
+	public KeyCode DownKey;
+	public KeyCode LeftKey;
+	public KeyCode RightKey;
 
+	void Start ()
+	{
+		JelloMgr.inst.Register (this);
+	}
 
-	// Use this for initialization
-	void Start () {
+	void Update ()
+	{
+		MovementLogic ();
+		ChopTrees ();
+	}
 
-		JelloMgr.inst.Register(this);
+	void MovementLogic () {
+		Vector3 speed = Vector3.zero;
+
+		if (Input.GetKey (UpKey)) {
+			speed.z = 1;
+		}
+
+		if (Input.GetKey (DownKey)) {
+			speed.z = -1;
+		}
+
+		if (Input.GetKey (LeftKey)) {
+			speed.x = -1;
+		}
+
+		if (Input.GetKey (RightKey)) {
+			speed.x = 1;
+		}
+
+		transform.position = transform.position + speed * Time.deltaTime;
+	}
+
+	void ChopTrees() {
+		// for each tree find distance from my jello to tree
+		// if distance is less than X, chop the tree down
+		// add wood to jelllo
+
+		foreach (Tree tree in TreeMgr.inst.AllTrees) {
+			Vector3 toTree = tree.transform.position - transform.position;
+			float distance = toTree.magnitude;
+			Debug.Log (distance.ToString());
+
+			if (distance <= 1.5f) {
+				tree.GetChopped ();
+			}
+		}
+
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
--- Assets/Scripts/Tree.cs
+++ Assets/Scripts/Tree.cs
@@ -4,14 +4,28 @@ using UnityEngine;
 
 public class Tree : MonoBehaviour {
 
+	bool AmIChopped = false;
+
+	public GameObject TreeView;
+	public GameObject WoodView;
+
 	// Use this for initialization
 	void Start () {
 
 		TreeMgr.inst.Register(this);
+		TreeView.SetActive (true);
+		WoodView.SetActive (false);
 	}
 
 	// Update is called once per frame
 	void Update () {
+
+	}
+
+	public void GetChopped() {
+		AmIChopped = true;
+		WoodView.SetActive (true);
+		TreeView.SetActive (false);
 
 	}
 }
