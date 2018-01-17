359a4b4ef5d01658b90f2da912c4142f74e5b119
emontecalvo <emilymontecalvo@gmail.com>
2018-01-17 09:23:00 -0800

    adding storm

--- Assets/Scripts/Thermometer.cs
+++ Assets/Scripts/Thermometer.cs
@@ -20,6 +20,12 @@ public class Thermometer : MonoBehaviour {
 	public float fillAmount;
 	public float randomFill;
 
+	public float StormOneStartTime;
+	public float StormOneEndTime;
+	public float StormTwoStartTime;
+	public float StormTwoEndTime;
+	public float LastTime;
+
 	void Start () {
 		
 	}
@@ -29,5 +35,37 @@ public class Thermometer : MonoBehaviour {
 		randomFill = Mathf.Cos (Time.time) * 0.05f;
 		float totalFill = fillAmount + randomFill;
 		ThermoFillImg.fillAmount = totalFill;
+//		Debug.Log ("Time dot time:" + Time.time.ToString () + "Time delta:" + Time.deltaTime.ToString ());
+
+
+
+		float time = Time.time;
+		if (time > StormOneStartTime && LastTime < StormOneStartTime) {
+			BeginStorm ();
+		}
+			
+		if (time > StormOneEndTime && LastTime < StormOneEndTime) {
+			EndStorm ();
+		}
+
+		if (time > StormTwoStartTime && LastTime < StormTwoStartTime) {
+			BeginStorm ();
+		}
+
+		if (time > StormTwoEndTime && LastTime < StormTwoEndTime) {
+			EndStorm ();
+		}
+
+		LastTime = time;
+
+	}
+
+	void BeginStorm() {
+		Debug.Log ("BEGIN STORM!");
+
+	}
+
+	void EndStorm () {
+		Debug.Log ("You survived, end of storm");
 	}
 }
